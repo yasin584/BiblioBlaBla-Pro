de48d1c3a4c2246763ffed497d1c786bf676d8f4
yasin584 <yassune24@live.com>
2026-03-23 12:44:17 +0100

    bestanden

---
+++ back-end/src/main/java/nl/biblioblabla/pro/BiblioBlaBlaProApplication.java
@@ -0,0 +1,13 @@
+package nl.biblioblabla.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioBlaBlaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioBlaBlaProApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/nl/biblioblabla/pro/BiblioBlaBlaProApplicationTests.java
@@ -0,0 +1,14 @@
+package nl.biblioblabla.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioBlaBlaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
