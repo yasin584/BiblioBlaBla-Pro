e8d27ef520e292d84164b455a40ca252a896d691
yasin584 <yassune24@live.com>
2026-03-27 11:14:26 +0100

    database aangepast

---
+++ back-end/src/main/java/nl/biblioblabla/pro/model/Leningen.java
@@ -0,0 +1,23 @@
+package nl.biblioblabla.pro.model;
+
+public class Author {
+    private int id;
+    private String naam;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+}
---
+++ back-end/src/main/java/nl/biblioblabla/pro/repository/LeningenRepository.java
@@ -0,0 +1,4 @@
+package nl.biblioblabla.pro.repository;
+
+public class LeningenRepository {
+}
