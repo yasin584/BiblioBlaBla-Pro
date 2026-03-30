9e77e9a3121c548b589f48ce966831ecfc88fd4e
FleurVdH <FAP.vanderHeijden@student.han.nl>
2026-03-30 10:37:08 +0200

    update UserRepository

--- back-end/src/main/java/nl/biblioblabla/pro/repository/UserRepository.java
+++ back-end/src/main/java/nl/biblioblabla/pro/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package nl.biblioblabla.pro.repository;
 
+import nl.biblioblabla.pro.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@repository
 public class UserRepository {
+
+    @Autowired
+    private jdbcTemplate jdbcTemplate;
     
 }
