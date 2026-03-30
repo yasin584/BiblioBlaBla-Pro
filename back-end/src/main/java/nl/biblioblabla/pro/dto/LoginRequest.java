439dfb7e9401467cddf59e0ba709c5eab3a1b6ee
FleurVdH <FAP.vanderHeijden@student.han.nl>
2026-03-30 10:46:06 +0200

    Update lombok
    Ik heb de lombok dependency toegevoegd

--- back-end/src/main/java/nl/biblioblabla/pro/dto/LoginRequest.java
+++ back-end/src/main/java/nl/biblioblabla/pro/dto/LoginRequest.java
@@ -1,37 +1,13 @@
 package nl.biblioblabla.pro.dto;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String wachtwoord;
-
-    public LoginRequest() {
-    }
-
-    public LoginRequest(String email, String wachtwoord) {
-        this.email = email;
-        this.wachtwoord = wachtwoord;
-    }
-
-    // -- Getters --
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getWachtwoord() {
-        return wachtwoord;
-    }
-
-    // -- Setters --
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setWachtwoord(String wachtwoord) {
-        this.wachtwoord = wachtwoord;
-    }
 }
