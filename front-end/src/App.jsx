7b0e6f4711b4ca2324ad219bfd09cfd900092c06
yasin584 <yassune24@live.com>
2026-04-07 19:31:15 +0200

    tussen door opslaan

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,29 @@
-import Header  from "./components/Header"
-import Login  from "./components/Login"
-function App() {
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+//import Leesgeschiedenis from "./leesgeschiedenis"; 
+import ProtectedRoute from "./components/Protectedrout";
 
-
+const App = () => {
   return (
-    <>
-    <Header/>
-    <Login/>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        {/* Iedereen heeft toegang */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        
+        {/* Alleen ingelogde gebruikers hebben toegang */}
+        <Route 
+          path="/leesgeschiedenis" 
+          element={
+            <ProtectedRoute>
+              {/* <Leesgeschiedenis /> */}
+            </ProtectedRoute>
+          } 
+        />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
