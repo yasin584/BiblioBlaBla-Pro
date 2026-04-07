db3773fbd02370c0a698639247e806230b35e52c
FleurVdH <FAP.vanderHeijden@student.han.nl>
2026-04-07 11:08:19 +0200

    Update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./login";
+import leesgeschiedenis from "./leesgeschiedenis";
 
-function App() {
-
-
+const App = () => {
   return (
-    <>
-    <div></div>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+
+          {/*iedereen heeft toegang*/}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
+
+
+          {/* alleen ingelogde gebruiker heeft toegang*/}
+              <Route path="/leesgeschiedenis" element={<leesgeschiedenis />} />      
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App
