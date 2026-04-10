e5f3958470228bcba48ba1e5a33d17b8cbb4d7fd
FleurVdH <FAP.vanderHeijden@student.han.nl>
2026-04-10 11:38:43 +0200

    update

--- front-end/src/stories/components/Filterbar.stories.jsx
+++ front-end/src/stories/components/Filterbar.stories.jsx
@@ -4,45 +4,19 @@ import FilterBar from '../../components/FilterBar';
 export default {
   title: 'Bibliotheek/FilterBar',
   component: FilterBar,
-  parameters: {
-    docs: {
-      description: {
-        component: 'Filterbalk om leningen te doorzoeken op titel, genre en datum.',
-      },
-    },
-  },
 };
 
 const defaultFilters = {
   titel: '',
+  auteur: '',
   genre: 'ALLE GENRES',
   start: '',
 };
 
 export const Leeg = () => {
   const [filters, setFilters] = useState(defaultFilters);
-  return (
-    <div>
-      <FilterBar filters={filters} setFilters={setFilters} />
-      <pre style={{ marginTop: '1rem', fontSize: '12px', background: '#f3f4f6', padding: '0.75rem', borderRadius: '6px' }}>
-        {JSON.stringify(filters, null, 2)}
-      </pre>
-    </div>
-  );
-};
 
-export const VooringevuldFilters = () => {
-  const [filters, setFilters] = useState({
-    titel: 'Harry Potter',
-    genre: 'Fantasy',
-    start: '2024-01-01',
-  });
   return (
-    <div>
-      <FilterBar filters={filters} setFilters={setFilters} />
-      <pre style={{ marginTop: '1rem', fontSize: '12px', background: '#f3f4f6', padding: '0.75rem', borderRadius: '6px' }}>
-        {JSON.stringify(filters, null, 2)}
-      </pre>
-    </div>
+    <FilterBar filters={filters} setFilters={setFilters} />
   );
 };
